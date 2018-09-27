73e4ed28b9aeeea5745905bf7778989d58325acd
ava krishn <avakrishn@gmail.com>
2018-09-26 17:52:10 -0700

    add click events for all articles and saved articles buttons and add click events for star

--- app/public/assets/javascript/app.js
+++ app/public/assets/javascript/app.js
@@ -1,9 +1,58 @@
 $('.section').on('click', function(){
     event.preventDefault();
-    var action = '/scrape/' + $(this).attr('id');
+    if($(this).attr('id') == 'all'){
+        $(this).parent().attr({
+            method: "GET",
+            action: '/'
+        });
+        $(this).parent().submit();
+
+    }else{
+        var action = '/scrape/' + $(this).attr('id');
+        $(this).parent().attr({
+            method: "POST",
+            action: action
+        });
+        $(this).parent().submit();
+    }
+});
+
+$('#all').on('click', function(){
+    event.preventDefault();
     $(this).parent().attr({
-        method: "POST",
-        action: action
+        method: "GET",
+        action: '/'
     });
     $(this).parent().submit();
-})
+});
+
+$('#saved').on('click', function(){
+    event.preventDefault();
+    $(this).parent().attr({
+        method: "GET",
+        action: '/saved'
+    });
+    $(this).parent().submit();
+});
+
+$(document).ready(function(){
+    $('.star').on('click', function(){
+        if($(this).hasClass('far')){
+            $(this).removeClass('far');
+            $(this).addClass('fas selected');
+            var action = "/add/star/" + $(this).attr("id");
+        }
+        else{
+            $(this).removeClass('fas selected');
+            $(this).addClass('far');
+            var action = "/delete/star/" + $(this).attr("id");
+        }
+        $(this).parent().attr({
+            method: "POST",
+            action: action
+        });
+        $(this).parent().submit();
+
+    })
+
+});
